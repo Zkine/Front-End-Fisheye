25a49e785362386273f49333e1f701971eb9a80d
Zkine <philippe_jaymes@hotmail.com>
2023-08-18 21:07:06 +0200

    la lightbox à maintenant des liens sur les buttons de navigation

--- js/templates/photographer.js
+++ js/templates/photographer.js
@@ -1,23 +1,36 @@
 const url = new URL(window.location.href);
 const id = url.searchParams.get("id");
-let mediaLightbox = [];
-let mediaTitleLightbox = [];
 
 class Lightbox {
+  static MediaAllSetect(Media, MediaTitleMap, MediaItems, MediaTitle) {
+    const linkMedia = document.querySelector("#media-lightbox-id");
+    console.log(linkMedia);
+    const mediaAll = Array.from(document.querySelectorAll("#item-media-id"));
+    Media = mediaAll.map(
+      (e) => e.getAttribute("src") || e.children[0].getAttribute("src")
+    );
+    MediaTitleMap = mediaAll.map(
+      (e) => e.getAttribute("alt") || e.children[0].getAttribute("alt")
+    );
+
+    MediaItems = Media.findIndex(
+      (element) => element === linkMedia.attributes[1].textContent
+    );
+
+    MediaTitle = MediaTitleMap.findIndex(
+      (element) => element === linkMedia.attributes[2].textContent
+    );
+
+    return [Media, MediaTitleMap, MediaItems, MediaTitle];
+  }
   constructor(data) {
     this._data = data;
-    if (this._data.MediaItems && this._data.MediaItems.includes("jpg")) {
-      mediaLightbox.push(this._data.MediaItems);
-      mediaTitleLightbox.push(this._data._MediaTitle);
-    } else if (this._data.MediaItems && this._data.MediaItems.includes("mp4")) {
-      mediaLightbox.push(this._data.MediaItems);
-      mediaTitleLightbox.push(this._data._MediaTitle);
-    }
   }
 
   //création du DOM de la lightbox
-  static DomLightbox(e) {
+  static async DomLightbox(e) {
     e.stopPropagation();
+
     const selectDomLightbox = document.getElementById("lightbox-modal");
     const section = document.createElement("section");
     section.classList.add("section-lightbox");
@@ -50,7 +63,6 @@ class Lightbox {
 
       const video = document.createElement("video");
       video.classList.add("media-lightbox");
-      video.id = "item-media-id";
       video.setAttribute("controls", "");
       figureLightbox.insertAdjacentElement("afterbegin", video);
 
@@ -66,14 +78,37 @@ class Lightbox {
       figcaptionVideo.textContent = `${videoAlt}`;
       figureLightbox.appendChild(figcaptionVideo);
     }
+    const [media, mediaTitleMap, MediaItems, MediaTitle] =
+      Lightbox.MediaAllSetect(e);
+    let mediaItems = MediaItems;
+    let mediaTitle = MediaTitle;
+    if (
+      mediaItems === media.length - 1 ||
+      mediaTitle === mediaTitleMap.length - 1
+    ) {
+      mediaItems = -1;
+      mediaTitle = -1;
+    }
     const previousLink = document.createElement("a");
     previousLink.setAttribute("type", "button");
+    if (media[mediaItems - 1]) {
+      previousLink.setAttribute("href", `${media[mediaItems - 1]}`);
+    } else if (media[mediaItems - 1] === undefined && mediaItems === 0) {
+      mediaItems = media.length;
+      previousLink.setAttribute("href", `${media[mediaItems - 1]}`);
+      mediaItems = 0;
+    } else if (media[mediaItems - 1] === undefined && mediaItems === -1) {
+      mediaItems = media.length;
+      previousLink.setAttribute("href", `${media[mediaItems - 2]}`);
+      mediaItems = -1;
+    }
     previousLink.classList.add("previous-link");
     previousLink.id = "previous-link-id";
     section.appendChild(previousLink);
 
     const nextLink = document.createElement("a");
     nextLink.setAttribute("type", "button");
+    nextLink.setAttribute("href", `${media[mediaItems + 1]}`);
     nextLink.classList.add("next-link");
     nextLink.id = "next-link-id";
     section.appendChild(nextLink);
@@ -100,123 +135,191 @@ class Lightbox {
   static nextImg(e) {
     e.preventDefault();
     e.stopPropagation();
-    //récupération du lien de l'image affichée dans la constante linkMedia
-    const imgParent = e.target.closest("#section-lightbox-id");
-    const linkMedia = imgParent.querySelector("#media-lightbox-id");
-
-    let mediaItems = mediaLightbox.findIndex(
-      (element) => element === linkMedia.attributes[1].textContent
-    );
-
-    let mediaTitle = mediaTitleLightbox.findIndex(
-      (element) => element === linkMedia.attributes[2].textContent
-    );
-
+    // récupération du lien de l'image affichée dans la constante linkMedia
+    const sectionLithtbox = e.target.closest("#section-lightbox-id");
+    const [media, mediaTitleMap, MediaItems, MediaTitle] =
+      Lightbox.MediaAllSetect(e);
+    let mediaItems = MediaItems;
+    let mediaTitle = MediaTitle;
     if (
-      mediaItems === mediaLightbox.length - 1 ||
-      mediaTitle === mediaTitleLightbox.length - 1
+      mediaItems === media.length - 1 ||
+      mediaTitle === mediaTitleMap.length - 1
     ) {
       mediaItems = -1;
       mediaTitle = -1;
     }
 
-    const figureLightbox = imgParent.querySelector("#figure-lightbox-id");
-    if (mediaLightbox[mediaItems + 1].includes("jpg")) {
+    sectionLithtbox.innerHTML = "";
+    const figureLightbox = document.createElement("figure");
+    figureLightbox.classList.add("figure-lightbox");
+    figureLightbox.id = "figure-lightbox-id";
+    sectionLithtbox.appendChild(figureLightbox);
+    if (media[mediaItems + 1].includes("jpg")) {
       const newImg = document.createElement("img");
       newImg.classList.add("media-lightbox");
-      newImg.setAttribute("src", `${mediaLightbox[mediaItems + 1]}`);
-      newImg.setAttribute("alt", `${mediaTitleLightbox[mediaTitle + 1]}`);
+      newImg.setAttribute("src", `${media[mediaItems + 1]}`);
+      newImg.setAttribute("alt", `${mediaTitleMap[mediaTitle + 1]}`);
       newImg.id = "media-lightbox-id";
       figureLightbox.insertAdjacentElement("afterbegin", newImg);
 
       const newfigcaption = document.createElement("figcaption");
       newfigcaption.classList.add("figcaption-lightbox");
-      newfigcaption.textContent = `${mediaTitleLightbox[mediaTitle + 1]}`;
+      newfigcaption.textContent = `${mediaTitleMap[mediaTitle + 1]}`;
       figureLightbox.appendChild(newfigcaption);
-
-      figureLightbox.innerHTML = "";
-      return figureLightbox.append(newImg, newfigcaption);
-    } else if (mediaLightbox[mediaItems + 1].includes("mp4")) {
+    } else if (media[mediaItems + 1].includes("mp4")) {
       const video = document.createElement("video");
       video.classList.add("media-lightbox");
-      video.id = "item-media-id";
       video.setAttribute("controls", "");
       figureLightbox.insertAdjacentElement("afterbegin", video);
 
       const source = document.createElement("source");
       source.id = "media-lightbox-id";
-      source.setAttribute("src", `${mediaLightbox[mediaItems + 1]}`);
-      source.setAttribute("alt", `${mediaTitleLightbox[mediaTitle + 1]}`);
+      source.setAttribute("src", `${media[mediaItems + 1]}`);
+      source.setAttribute("alt", `${mediaTitleMap[mediaTitle + 1]}`);
       source.setAttribute("type", "video/mp4");
       video.appendChild(source);
 
       const figcaptionVideo = document.createElement("figcaption");
       figcaptionVideo.classList.add("figcaption-lightbox");
-      figcaptionVideo.textContent = `${mediaTitleLightbox[mediaTitle + 1]}`;
+      figcaptionVideo.textContent = `${mediaTitleMap[mediaTitle + 1]}`;
       figureLightbox.appendChild(figcaptionVideo);
-      figureLightbox.innerHTML = "";
-      return figureLightbox.append(video, figcaptionVideo);
     }
+    const previousLink = document.createElement("a");
+    previousLink.setAttribute("type", "button");
+    if (media[mediaItems - 0]) {
+      previousLink.setAttribute("href", `${media[mediaItems - 0]}`);
+    } else if (media[mediaItems - 0] === undefined && media.length - 2) {
+      mediaItems = media.length;
+      previousLink.setAttribute("href", `${media[mediaItems - 1]}`);
+    }
+
+    previousLink.classList.add("previous-link");
+    previousLink.id = "previous-link-id";
+    sectionLithtbox.appendChild(previousLink);
+
+    const nextLink = document.createElement("a");
+    nextLink.setAttribute("type", "button");
+    if (media[mediaItems + 2]) {
+      nextLink.setAttribute("href", `${media[mediaItems + 2]}`);
+    } else if (
+      media[mediaItems + 2] === undefined &&
+      mediaItems == media.length - 2
+    ) {
+      mediaItems = -1;
+      nextLink.setAttribute("href", `${media[mediaItems + 1]}`);
+    } else if (
+      media[mediaItems + 2] === undefined &&
+      mediaItems === media.length
+    ) {
+      mediaItems = -1;
+      nextLink.setAttribute("href", `${media[mediaItems + 2]}`);
+    }
+    nextLink.classList.add("next-link");
+    nextLink.id = "next-link-id";
+    sectionLithtbox.appendChild(nextLink);
+
+    const buttonModal = document.createElement("button");
+    buttonModal.classList.add("button-modal");
+    sectionLithtbox.appendChild(buttonModal);
+
+    buttonModal.addEventListener("click", Lightbox.lightboxClose.bind(this));
+    nextLink.addEventListener("click", Lightbox.nextImg.bind(this));
+    previousLink.addEventListener("click", Lightbox.prevImg.bind(this));
+
+    return sectionLithtbox;
   }
 
   static prevImg(e) {
     e.preventDefault();
     e.stopPropagation();
     //récupération du lien de l'image affichée dans la constante linkMedia
-    const imgParent = e.target.closest("#section-lightbox-id");
-    const linkMedia = imgParent.querySelector("#media-lightbox-id");
-
-    let mediaItems = mediaLightbox.findIndex(
-      (element) => element === linkMedia.attributes[1].textContent
-    );
-
-    let mediaTitle = mediaTitleLightbox.findIndex(
-      (element) => element === linkMedia.attributes[2].textContent
-    );
-
+    const sectionLithtbox = e.target.closest("#section-lightbox-id");
+    const [media, mediaTitleMap, MediaItems, MediaTitle] =
+      Lightbox.MediaAllSetect(e);
+    let mediaItems = MediaItems;
+    let mediaTitle = MediaTitle;
     if (mediaItems === 0 || mediaTitle === 0) {
-      mediaItems = mediaLightbox.length - 1;
-      mediaTitle = mediaTitleLightbox.length - 1;
+      mediaItems = media.length;
+      mediaTitle = mediaTitleMap.length;
     }
 
-    const figureLightbox = imgParent.querySelector("#figure-lightbox-id");
-
-    if (mediaLightbox[mediaItems - 1].includes("jpg")) {
+    sectionLithtbox.innerHTML = "";
+    const figureLightbox = document.createElement("figure");
+    figureLightbox.classList.add("figure-lightbox");
+    figureLightbox.id = "figure-lightbox-id";
+    sectionLithtbox.appendChild(figureLightbox);
+    if (media[mediaItems - 1].includes("jpg")) {
       const newImg = document.createElement("img");
       newImg.classList.add("media-lightbox");
-      newImg.setAttribute("src", `${mediaLightbox[mediaItems - 1]}`);
-      newImg.setAttribute("alt", `${mediaTitleLightbox[mediaTitle - 1]}`);
+      newImg.setAttribute("src", `${media[mediaItems - 1]}`);
+      newImg.setAttribute("alt", `${mediaTitleMap[mediaTitle - 1]}`);
       newImg.id = "media-lightbox-id";
       figureLightbox.insertAdjacentElement("afterbegin", newImg);
 
       const newfigcaption = document.createElement("figcaption");
       newfigcaption.classList.add("figcaption-lightbox");
-      newfigcaption.textContent = `${mediaTitleLightbox[mediaTitle - 1]}`;
+      newfigcaption.textContent = `${mediaTitleMap[mediaTitle - 1]}`;
       figureLightbox.insertAdjacentElement("beforeend", newfigcaption);
-
-      figureLightbox.innerHTML = "";
-      return figureLightbox.append(newImg, newfigcaption);
-    } else if (mediaLightbox[mediaItems - 1].includes("mp4")) {
+    } else if (media[mediaItems - 1].includes("mp4")) {
       const video = document.createElement("video");
       video.classList.add("media-lightbox");
-      video.id = "item-media-id";
       video.setAttribute("controls", "");
       figureLightbox.insertAdjacentElement("afterbegin", video);
 
       const source = document.createElement("source");
       source.id = "media-lightbox-id";
-      source.setAttribute("src", `${mediaLightbox[mediaItems - 1]}`);
-      source.setAttribute("alt", `${mediaTitleLightbox[mediaTitle - 1]}`);
+      source.setAttribute("src", `${media[mediaItems - 1]}`);
+      source.setAttribute("alt", `${mediaTitleMap[mediaTitle - 1]}`);
       source.setAttribute("type", "video/mp4");
       video.appendChild(source);
 
       const figcaptionVideo = document.createElement("figcaption");
       figcaptionVideo.classList.add("figcaption-lightbox");
-      figcaptionVideo.textContent = `${mediaTitleLightbox[mediaTitle - 1]}`;
+      figcaptionVideo.textContent = `${mediaTitleMap[mediaTitle - 1]}`;
       figureLightbox.appendChild(figcaptionVideo);
-      figureLightbox.innerHTML = "";
-      return figureLightbox.append(video, figcaptionVideo);
     }
+    const previousLink = document.createElement("a");
+    previousLink.setAttribute("type", "button");
+    if (media[mediaItems - 2]) {
+      previousLink.setAttribute("href", `${media[mediaItems - 2]}`);
+    } else if (media[mediaItems - 2] === undefined && mediaItems === 1) {
+      mediaItems = media.length;
+      previousLink.setAttribute("href", `${media[mediaItems - 1]}`);
+      mediaItems = 1;
+    }
+    previousLink.classList.add("previous-link");
+    previousLink.id = "previous-link-id";
+    sectionLithtbox.appendChild(previousLink);
+
+    const nextLink = document.createElement("a");
+    nextLink.setAttribute("type", "button");
+    if (media[mediaItems + 0]) {
+      nextLink.setAttribute("href", `${media[mediaItems + 0]}`);
+    } else if (
+      media[mediaItems + 0] === undefined &&
+      mediaItems === media.length
+    ) {
+      mediaItems = -1;
+      nextLink.setAttribute("href", `${media[mediaItems + 1]}`);
+    } else if (
+      media[mediaItems + 0] === undefined &&
+      mediaItems === media.length - 1
+    ) {
+      nextLink.setAttribute("href", `${media[mediaItems + 1]}`);
+    }
+    nextLink.classList.add("next-link");
+    nextLink.id = "next-link-id";
+    sectionLithtbox.appendChild(nextLink);
+
+    const buttonModal = document.createElement("button");
+    buttonModal.classList.add("button-modal");
+    sectionLithtbox.appendChild(buttonModal);
+
+    buttonModal.addEventListener("click", Lightbox.lightboxClose.bind(this));
+    nextLink.addEventListener("click", Lightbox.nextImg.bind(this));
+    previousLink.addEventListener("click", Lightbox.prevImg.bind(this));
+
+    return sectionLithtbox;
   }
 }
 
